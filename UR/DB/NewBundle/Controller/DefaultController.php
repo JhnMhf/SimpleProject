45c8a25f71014c4d0e036b9a23f1b1ad905614b4
JhnMhf <johannameh91@gmail.com>
2016-03-26 00:30:47 +0100

    Added convenience function to clear database before/after tests.

--- UR/DB/NewBundle/Controller/DefaultController.php
+++ UR/DB/NewBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace UR\DB\NewBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -24,5 +25,51 @@ class DefaultController extends Controller
         }
 
         return $this->get("response_builder.service")->getJSONResponse($person);
+    }
+    
+    //http://stackoverflow.com/questions/5452760/truncate-foreign-key-constrained-table
+    public function clearDatabaseAction(){
+        $newDBManager = $this->get('doctrine')->getManager('new');
+        
+        $sql= "
+                SET FOREIGN_KEY_CHECKS = 0;             
+                TRUNCATE `baptism`;
+                TRUNCATE `birth`;
+                TRUNCATE `country`;
+                TRUNCATE `date`;
+                TRUNCATE `death`;
+                TRUNCATE `education`;
+                TRUNCATE `honour`;
+                TRUNCATE `is_grandparent`;
+                TRUNCATE `is_in_relationship_with`;
+                TRUNCATE `is_parent`;
+                TRUNCATE `is_parent_in_law`;
+                TRUNCATE `is_sibling`;
+                TRUNCATE `job`;
+                TRUNCATE `job_class`;
+                TRUNCATE `location`;
+                TRUNCATE `nation`;
+                TRUNCATE `partner`;
+                TRUNCATE `person`;
+                TRUNCATE `property`;
+                TRUNCATE `rank`;
+                TRUNCATE `relative`;
+                TRUNCATE `religion`;
+                TRUNCATE `residence`;
+                TRUNCATE `road_of_life`;
+                TRUNCATE `source`;
+                TRUNCATE `status`;
+                TRUNCATE `territory`;
+                TRUNCATE `unique_id_sequence`;
+                TRUNCATE `wedding`;
+                TRUNCATE `works`;
+                SET FOREIGN_KEY_CHECKS = 1;
+                ";
+        
+        $stmt = $newDBManager->getConnection()->prepare($sql);
+            
+        $stmt->execute();
+        
+        return new Response('Truncated the whole new database!');
     }
 }
