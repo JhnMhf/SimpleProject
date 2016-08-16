b60c6f5df83d5caf79b31da5aa49aa6029d44cd5
JhnMhf <johannameh91@gmail.com>
2016-08-16 23:21:15 +0200

    Added basis for displaying new/ final db persons.

--- UR/AmburgerBundle/Resources/public/js/src/datacorrection/person/NewPersonView.js
+++ UR/AmburgerBundle/Resources/public/js/src/datacorrection/person/NewPersonView.js
@@ -11,7 +11,21 @@ PersonCorrection.NewPersonView = (function(){
     },
     
     displayPerson = function(personData){
-        
+        displayBasePerson(personData);
+    },
+    
+    displayBasePerson = function(personData){
+        console.log(personData);
+        var template = _.template($("script#basePerson").html());
+        console.log("template", template);
+        $("#new").append(template({
+            oid:personData['oid'], 
+            firstName:personData['first_name'],
+            patronym:personData['patronym'],
+            lastName:personData['last_name'],
+            foreName:personData['fore_name'],
+            birthName:personData['birth_name']
+        }));
     };
 
     that.init = init;
