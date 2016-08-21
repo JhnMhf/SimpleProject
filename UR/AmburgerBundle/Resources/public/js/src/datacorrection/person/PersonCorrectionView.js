c91656f7c19bd6c6e7290b0bea26d569fc295c55
JhnMhf <johannameh91@gmail.com>
2016-08-21 13:02:48 +0200

    Made parts of the person correction collapsible.

--- UR/AmburgerBundle/Resources/public/js/src/datacorrection/person/PersonCorrectionView.js
+++ UR/AmburgerBundle/Resources/public/js/src/datacorrection/person/PersonCorrectionView.js
@@ -7,12 +7,40 @@ PersonCorrection.PersonCorrectionView = (function(){
     */
     init = function() {
         $('#save-button').on("click", save);
+        $('.collapsible').on("click", collapse);
         return that;
     },
     
     
     save = function(){
         $(that).trigger('save');
+    },
+    
+    collapse = function(args){
+        console.log(args);
+        
+        var clickedContainer = extractClickedContainer(args.currentTarget.className);
+        
+        console.log(clickedContainer);
+        
+        var elements = $("."+clickedContainer);
+        
+        if(elements.hasClass('collapsed')){
+            elements.removeClass('collapsed');
+        } else{
+            elements.addClass('collapsed');
+        }
+        
+        
+    },
+    
+    extractClickedContainer = function(classes){
+        var classList = classes.split(/\s+/);
+        for (i = 0; i < classList.length; i++) {
+           if (classList[i].includes('-container')) {
+               return classList[i];
+           }
+        }
     };
 
 
