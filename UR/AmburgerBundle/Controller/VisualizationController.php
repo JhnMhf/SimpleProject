ce91394cb6d3efb7dee5eaf36a4ccb1876f99d4f
JhnMhf <johannameh91@gmail.com>
2016-10-09 22:36:44 +0200

    Fixed second wrong column name.

--- UR/AmburgerBundle/Controller/VisualizationController.php
+++ UR/AmburgerBundle/Controller/VisualizationController.php
@@ -216,7 +216,7 @@ class VisualizationController extends Controller{
     }
     
     private function internalLoadLocationsForIds($personIds){
-        $sql = "SELECT id,name,latitude, longitude, locationData.sum FROM location
+        $sql = "SELECT id,location_name,latitude, longitude, locationData.sum FROM location
                 JOIN (SELECT locations.locationid as locationid,SUM(locations.c) as sum
                 FROM (
                   SELECT baptism_locationid as locationid,COUNT(*) AS c FROM baptism WHERE baptism_locationid IS NOT NULL 
